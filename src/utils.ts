2fe09ee109754364648e26e3372948cabc275738
Safin Singh <safin.singh@gmail.com>
2020-08-11 12:07:56 -0700

    Add types to everything in utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,36 @@
 import * as discord from 'discord.js'
-import * as codeforces from 'codeforces-api'
 
-import { warn } from './logger'
-
-export const failEmbed = (type, message): discord.MessageEmbed => {
+export const failEmbed = (
+  type: string,
+  message: string
+): discord.MessageEmbed => {
   return new discord.MessageEmbed()
     .setColor('#ff0000')
     .addFields({ name: type, value: message })
 }
 
-export const successEmbed = (type, message): discord.MessageEmbed => {
+export const successEmbed = (
+  type: string,
+  message: string
+): discord.MessageEmbed => {
   return new discord.MessageEmbed()
     .setColor('#00ff00')
     .addFields({ name: type, value: message })
 }
 
-export const warnEmbed = (type, message): discord.MessageEmbed => {
+export const warnEmbed = (
+  type: string,
+  message: string
+): discord.MessageEmbed => {
   return new discord.MessageEmbed()
     .setColor('#FFFF00')
     .addFields({ name: type, value: message })
 }
 
-export const ratingFailEmbed = (type, message): discord.MessageEmbed => {
+export const ratingFailEmbed = (
+  type: string,
+  message: string
+): discord.MessageEmbed => {
   return new discord.MessageEmbed()
     .setColor('#ff0000')
     .addFields({ name: type, value: message })
